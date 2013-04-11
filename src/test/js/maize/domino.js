785251d929419c83379d5484f9296cca94aafc5d
Daniele Guido <gui.daniele@gmail.com>
2013-04-11 10:56:12 +0200

    server connectivity verification

--- src/test/js/maize/domino.js
+++ src/test/js/maize/domino.js
@@ -29,12 +29,48 @@
 		_this.model = model;
 		_this.ns = ns;
 
+		/*
+			Print out the pseudo identifier for this jsonrpc wrapper
+			
+		*/
 		this.label = function(){
 			return typeof _this.ns == "undefined"? _this.model: [_this.ns, _this.model].join(".")
 		}
-		this.execute = function(params, callback){ maize.log( '<maize.domino.JsonRPCBuilder:' + _this.label() +'> execute]' , params );
-			$.jsonRPC.request( _this.model,{ params : params, success: callback, error: maize.fault });
+
+		/*
+			Execute the call, with jsonrpc params (usually a list) and a callback function.
+			A proceed adn a fault arguments are available (but not necessary) to hook json response validation.
+			Proceed function valudate the json result even if a "success" has been received.
+			Cfr the default proceed and fault function.
+
+			@param params -	list
+			@param callback - function
+			@param proceed - function (optional, override the existing one)
+			@param fault - funciton (optional, override the existing one)
+		*/
+		this.execute = function( params, callback, proceed, fault ){ maize.log( '<maize.domino.JsonRPCBuilder:' + _this.label() +'> execute]' , params );
+			$.jsonRPC.request( _this.model,{ 
+				params : params,
+				success: function( result ){ typeof proceed == "undefined"? _this.proceed( result, callback ):proceed( result, callback); }, 
+				error: typeof fault == "undefined"?  _this.fault: fault });
 		};
+
+		/*
+			Verify
+
+		*/
+		this.proceed = function( result, callback ){ maize.log( '<maize.domino.JsonRPCBuilder:' + _this.label() +'> proceed]' , result );
+			// proceed to callback, check result.status == 'ok' or similar @todo !
+			callback( result );
+		}
+
+		/*
+			REAL FAULT
+			(server not responding)
+		*/
+		this.fault =function( result ){ maize.log( '<maize.domino.JsonRPCBuilder:' + _this.label() +'> jsonrpc fault]' , data );
+			maize.fault( result.error );
+		}
 
 		maize.log( '<maize.domino.JsonRPCBuilder:' + _this.label() +'> init]', model );
 	};
