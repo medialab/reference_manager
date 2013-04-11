034eb408e8c21538918a66aa6c11a15f13510853
Daniele Guido <gui.daniele@gmail.com>
2013-04-11 18:19:26 +0200

    echo page
    some domino experiment

--- src/test/js/maize/domino.js
+++ src/test/js/maize/domino.js
@@ -1,10 +1,11 @@
-( function( $, w, undefined ){
+( function( $, w, domino, undefined ){
 	/*
 
 
 		Application dedicated domino
 		configuration and modules
 		Support Handlebars for template rendering.
+		Requirement: domino, maize and jquery
 
 	*/
 	var maize = w.maize || {};
@@ -30,14 +31,15 @@
 		_this.ns = ns;
 
 		/*
-			Print out the pseudo identifier for this jsonrpc wrapper
-			
+
+			Print out the pseudo identifier for this jsonrpc wrapper instance
 		*/
 		this.label = function(){
 			return typeof _this.ns == "undefined"? _this.model: [_this.ns, _this.model].join(".")
 		}
 
 		/*
+
 			Execute the call, with jsonrpc params (usually a list) and a callback function.
 			A proceed adn a fault arguments are available (but not necessary) to hook json response validation.
 			Proceed function valudate the json result even if a "success" has been received.
@@ -56,8 +58,8 @@
 		};
 
 		/*
-			Verify
 
+			Verify response when a success responce has been received.
 		*/
 		this.proceed = function( result, callback ){ maize.log( '<maize.domino.JsonRPCBuilder:' + _this.label() +'> proceed]' , result );
 			// proceed to callback, check result.status == 'ok' or similar @todo !
@@ -88,33 +90,144 @@
 		return _api;
 	}
 
+
+
 	maize.domino.ready = function(){ maize.log( '[maize.domino.ready]' );
 		$.jsonRPC.setup({ endPoint : 'http://localhost:8080', namespace : ''});
 		maize.domino.api = maize.domino.compile( maize.domino.urls );
 
-		$("#test-json").on("click", function(){ maize.domino.api.echo.execute([$("#echo_input").val()], function(data){maize.log(data); maize.toast( data.result, "received");}) });
+		$("#test-json").on("click", function( event ){ event.preventDefault(); maize.domino.api.echo.execute([$("#echo_input").val()], function(data){maize.log(data); maize.toast( data.result, "received");}) });
 
 		maize.d = new domino({
 			properties: [
-				
+				/*{
+					
+						Story. Basically, a collection of chapters :D
+						Increment its value to land somewhere.
+					
+					id: 'story',
+					label: 'the  right story',
+					value: [ 'introduction', 'the-hero' ],
+					type: 'array',
+					triggers: 'domino.story.ascend',
+					dispatch: 'domino.story.landed'
+				},
+				*/
+				{	
+					/*
+						Chapter. Basically, a stepper.
+						Increment its value to land somewhere.
+					*/
+					id: 'chapter',
+					label: 'story chapter',
+					value: '0',
+					type: 'string',
+					triggers: 'chapter.switch',
+					dispatch: 'chapter.landed'
+				}
 			], hacks:[
+				{
+					triggers: 'chapter.switch',
+					method: function( data ) {
+						maize.log( '[hack:chapter.switch]', data);
+						return;
+						if (!this.get('updating'))
+							this.request('getValue');
 
+						this.updating = false;
+					}
+				},
+				{
+					triggers: 'chapter.landed',
+					method: function( data ) {
+						maize.log( '[hack:chapter.landed]', data);
+						return;
+						if (!this.get('updating'))
+							this.request('getValue');
+
+						this.updating = false;
+					}
+				}
 			]
 		});
 
+		maize.domino.module.ready();
 	};
 
 	
 	/*
 
 
-			Domino Wrapper
-			==============
-
-
-	maize.domino = new domino({
-		properties: [
-		]
-	});	
+		Domino Modules
+		==============
 	*/
-})( jQuery, window );
+	maize.domino.module = {};
+	maize.domino.module.Chapter = function( options ) {
+		domino.module.call(this);
+		maize.log( '<maize.domino.module.Chapter>', options);
+		var self = this,
+		o = $.extend({
+			story:'',
+			index:'',
+			element:[] // jquery *empty* element
+		}, options);
+
+		this.html = o.element
+
+	};
+
+	maize.domino.module.ChapterLink = function( options ) {
+		domino.module.call(this);
+		maize.log( '<maize.domino.module.ChapterLink>', options);
+		var self = this,
+			o = $.extend({
+				value:'',
+				element:[] // jquery *empty* element
+			}, options);
+
+		this.html = o.element.on('click', function(){
+			maize.log( '<maize.domino.module.ChapterLink> on click', o.value);
+			self.dispatchEvent('chapter.switch', {'chapter':o.value});
+		});
+	}
+
+
+	maize.domino.module.ready = function(){
+		// create chapter
+		$("[data-domino-module=Chapter]").each( function(i, e){
+			
+			var el = $(this),
+				story = el.attr('data-story'),
+				index = el.attr('data-chapter-index')
+
+				// note: 'next' link can be overridden by children with data-chapter-next data-story-jump "Jumper" moudles
+				// next chapter event will be trigger from 
+			
+			maize.log( '[maize.domino.module.ready]', 'data-domino-module=Chapter',  index );
+			// a chapter *must* manifest a data-story and a data-chapter-index
+			
+			maize.d.addModule( maize.domino.module.Chapter, [{
+				value: index,
+				property: 'chapter',
+				element: el
+			}]);
+			
+		});
+
+		// setup chapter button behaviour
+		$("[data-domino-module=ChapterLink]").each( function(i, e){
+			var el = $(this),
+				target = el.attr('data-chapter-index');
+
+			maize.log( '[maize.domino.module.ready]', 'data-domino-module=ChapterLink',  target );
+
+			maize.d.addModule( maize.domino.module.ChapterLink, [{
+				value: target,
+				property: 'chapter',
+				element: el
+			}]);
+		});
+		
+	}
+
+})( jQuery, window, domino );
