8fbd13d97aac9f0238b69a7870612f7058dfe9de
jrault <julien.rault@sciences-po.fr>
2013-10-01 11:55:26 +0200

    Add usage comments to the start_jsonrpc.sh

--- start_jsonrpc.sh
+++ start_jsonrpc.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# usage: 
+# usage:
+# path = /var/opt/biblib/reference_manager
+# env = /home/someone/.virtualenv/BIBLIB/bin/
+# $path/start_jsonrpc.sh $env $path
+
+# AIME usage:
 # dev: /home/jra/reference_manager/start_jsonrpc.sh /home/jra/.virtualenv/AIME/bin/ /home/jra/reference_manager/
 # prodv2: /var/opt/biblib/reference_manager/start_jsonrpc.sh /home/jra/.virtualenv/AIMEPROD/bin/ /var/opt/biblib/reference_manager/
 # prodv3: /var/opt/biblib/prod_v3/start_jsonrpc.sh /home/jra/.virtualenv/AIMEPROD/bin/ /var/opt/biblib/prod_v3/
