e77c4d8070a9d5e180bc6f560836868d50ff98dc
jrault <julien.rault@sciences-po.fr>
2014-07-29 15:45:12 +0200

    Update oai-pmh target

--- biblib/spire2repec.py
+++ biblib/spire2repec.py
@@ -62,29 +62,29 @@ if __name__ == "__main__":
     target['identifier'] = 'spire'
     target['title'] = 'Sciences Po Institutional Repository'
     target['type'] = 'oaipmh'
-    target['url'] = 'http://spire.sciences-po.fr/dissemination/oaipmh2-publications.xml'
+    target['url'] = 'http://spire.sciencespo.fr/dissemination/oaipmh2-no-prefix-publications.xml'
     target['metadata_prefix'] = 'didl'
 
     ids = [
-        "oai:spire.sciences-po.fr:2441/dambferfb7dfprc9m26c8c8o3",
-        "oai:spire.sciences-po.fr:2441/eo6779thqgm5r489makgoai85",
-        "oai:spire.sciences-po.fr:2441/5l6uh8ogmqildh09h6m8hj429",
-        "oai:spire.sciences-po.fr:2441/3fm4jv3k2s99lms9jb5i5asil",
-        "oai:spire.sciences-po.fr:2441/f4rshpf3v1umfa09lb0joe5g5",
-        "oai:spire.sciences-po.fr:2441/dambferfb7dfprc9m2h2og5ig",
-        "oai:spire.sciences-po.fr:2441/53r60a8s3kup1vc9k0skmec8o",
-        "oai:spire.sciences-po.fr:2441/eo6779thqgm5r489matggisrp",
-        "oai:spire.sciences-po.fr:2441/eo6779thqgm5r489m6ock14ik",
-        "oai:spire.sciences-po.fr:2441/eo6779thqgm5r489mam352ps9",
-        "oai:spire.sciences-po.fr:2441/eo6779thqgm5r489m4m2k6d4l",
-        "oai:spire.sciences-po.fr:2441/7t638g2jjsquujs9mcc0mcg2k",
-        "oai:spire.sciences-po.fr:2441/eu4vqp9ompqllr09hh1amcck6",
-        "oai:spire.sciences-po.fr:2441/c8dmi8nm4pdjkuc9g8mb1088n",
-        "oai:spire.sciences-po.fr:2441/5rkqqmvrn4tl22s9mc0sob64l",
-        "oai:spire.sciences-po.fr:2441/eu4vqp9ompqllr09hd5rha0il",
-        "oai:spire.sciences-po.fr:2441/eu4vqp9ompqllr09ij4jnag10",
-        "oai:spire.sciences-po.fr:2441/dambferfb7dfprc9lj6bo200k",
-        "oai:spire.sciences-po.fr:2441/c6t1fl36hv9s7q89j8pa2cp3i"
+        "oai:spire.sciencespo.fr:2441/dambferfb7dfprc9m26c8c8o3",
+        "oai:spire.sciencespo.fr:2441/eo6779thqgm5r489makgoai85",
+        "oai:spire.sciencespo.fr:2441/5l6uh8ogmqildh09h6m8hj429",
+        "oai:spire.sciencespo.fr:2441/3fm4jv3k2s99lms9jb5i5asil",
+        "oai:spire.sciencespo.fr:2441/f4rshpf3v1umfa09lb0joe5g5",
+        "oai:spire.sciencespo.fr:2441/dambferfb7dfprc9m2h2og5ig",
+        "oai:spire.sciencespo.fr:2441/53r60a8s3kup1vc9k0skmec8o",
+        "oai:spire.sciencespo.fr:2441/eo6779thqgm5r489matggisrp",
+        "oai:spire.sciencespo.fr:2441/eo6779thqgm5r489m6ock14ik",
+        "oai:spire.sciencespo.fr:2441/eo6779thqgm5r489mam352ps9",
+        "oai:spire.sciencespo.fr:2441/eo6779thqgm5r489m4m2k6d4l",
+        "oai:spire.sciencespo.fr:2441/7t638g2jjsquujs9mcc0mcg2k",
+        "oai:spire.sciencespo.fr:2441/eu4vqp9ompqllr09hh1amcck6",
+        "oai:spire.sciencespo.fr:2441/c8dmi8nm4pdjkuc9g8mb1088n",
+        "oai:spire.sciencespo.fr:2441/5rkqqmvrn4tl22s9mc0sob64l",
+        "oai:spire.sciencespo.fr:2441/eu4vqp9ompqllr09hd5rha0il",
+        "oai:spire.sciencespo.fr:2441/eu4vqp9ompqllr09ij4jnag10",
+        "oai:spire.sciencespo.fr:2441/dambferfb7dfprc9lj6bo200k",
+        "oai:spire.sciencespo.fr:2441/c6t1fl36hv9s7q89j8pa2cp3i"
     ]
     harvest_by_ids(corpus, target, ids)
     #harvest_by_set(corpus, target, "SHS:STAT")
